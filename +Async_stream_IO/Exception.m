c118d5170200f3e9a2d377e3e35494cd98833dc5
埃博拉酱 <vhtmfu@outlook.com>
2025-07-21 21:30:37 +0800

    20250721

--- +Async_stream_IO/Exception.m
+++ +Async_stream_IO/Exception.m
@@ -1,11 +1,7 @@
 classdef Exception<uint8&MATLAB.Lang.IEnumerableException
 	enumeration
 		Success(0)
-		Function_runtime_error(1)
-		Port_idle(2)
-		Port_occupied(3)
-		Argument_message_incomplete(4)
-		Corrupted_object_received(5)
-		Message_received_on_allocated_port(6)
+		Argument_message_incomplete(1)
+		Corrupted_object_received(2)
 	end
 end

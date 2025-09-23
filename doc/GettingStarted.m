afdb17d646f5cdd442dba2c7e40291131430761e
埃博拉酱 <vhtmfu@outlook.com>
2025-09-23 10:47:48 +0800

    测试手动安装库

--- doc/GettingStarted.m
+++ doc/GettingStarted.m
@@ -6,10 +6,7 @@
 %[text] - 安装了 Arduino IDE 2.0.0 以上版本
 %[text] - 在IDE中安装了开发板 Arduino SAM Boards （Due开发板，推荐）或 Arduino AVR Boards （Mega开发板，不推荐，内存偏小），和库Timers\_one\_for\_all（v3.0.0+）、Quick\_digital\_IO\_interrupt（v3.0.0+）及它们的所有依赖库 \
 %[text] 如果 Arduino IDE 库管理器中未列出Timers\_one\_for\_all和Quick\_digital\_IO\_interrupt，可选手动安装（应优先用 Arduino IDE 安装，不行再手动：
-if questdlg('在 Arduino IDE 中尝试安装了Timers_one_for_all（v3.0.0+）和Quick_digital_IO_interrupt（v3.0.0+）吗？','请确认','成功了','失败了','成功了')=="失败了"
-	libsDir = fullfile(getenv('USERPROFILE'), 'Documents\Arduino\libraries');
-	warning off MATLAB:MKDIR:DirectoryExists;
-	mkdir(libsDir);
+if questdlg('在 Arduino IDE 中尝试安装了Timers_one_for_all（v3.0.0+）和Quick_digital_IO_interrupt（v3.0.0+）吗？','请确认','成功了','失败了','还没试','成功了')=="失败了"
 	InstallLibrary('Timers_one_for_all', '3.0.0');
 	InstallLibrary('Quick_digital_IO_interrupt', '3.0.0');
 end
@@ -22,13 +19,29 @@ Gbec.Setup; %[output:21bce9c1]
 MATLAB.IO.Delete(fullfile(userpath,'+Gbec'));
 %%
 function InstallLibrary(Name,Version)
-NameVersion = sprintf('%s-v%s', Name, Version);
+persistent libsDir
+if isempty(libsDir)
+	libsDir = fullfile(getenv('USERPROFILE'), 'Documents\Arduino\libraries');
+end
+warning off MATLAB:MKDIR:DirectoryExists;
+mkdir(libsDir);
+NameVersion = sprintf('%s-%s', Name, Version);
 zipPath = fullfile(tempdir,NameVersion+".zip");
 if ~isfile(zipPath)
 	websave(zipPath, sprintf('https://github.com/Ebola-Chan-bot/%s/archive/refs/tags/v%s.zip', Name, Version));
 end
 unzip(zipPath, libsDir);
-MATLAB.IO.MoveFile(fullfile(libsDir,NameVersion), fullfile(libsDir,Name));
+UnzipPath = fullfile(libsDir,NameVersion);
+TargetPath=fullfile(libsDir,Name);
+try
+	MATLAB.IO.MoveFile(UnzipPath, TargetPath);
+catch ME
+	if ME.identifier=="MATLAB:Exception:File_operation_failed"
+		MATLAB.IO.Delete(UnzipPath);
+	else
+		ME.rethrow;
+	end
+end
 end
 
 %[appendix]{"version":"1.0"}

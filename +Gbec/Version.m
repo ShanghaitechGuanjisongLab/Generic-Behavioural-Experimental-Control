4fd9f9536d1d50af96022aacd7dd825f8e2a916f
埃博拉酱 <vhtmfu@outlook.com>
2025-10-09 23:52:33 +0800

    部署版本需要+1

--- +Gbec/Version.m
+++ +Gbec/Version.m
@@ -2,7 +2,7 @@ function V=Version
 V.Me='v7.0.0';
 V.MatlabExtension='18.1.0';
 V.MATLAB='R2025a';
-V.Deploy=10;
+V.Deploy=11;
 persistent NewVersion
 if isempty(NewVersion)
 	NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/ShanghaitechGuanjisongLab/Generic-Behavioural-Experimental-Control/releases','通用行为实验控制',V.Me);
